24dd8ed5bc34c1d506b703bbc599d2d699fbdcc9
federicovagnoni <fede93.vagnoni@gmail.com>
2017-07-17 15:09:08 +0200

    Fixes in navigation

--- src/main/resources/static/js/measure-task.js
+++ src/main/resources/static/js/measure-task.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 
     $.ajax({
 
-        url: getPhase4URL() + "/validation/measureTasks?businessWorkflowProcessDefinitionId="+
+        url: getPhase3URL() + "/validation/measureTasks?businessWorkflowProcessDefinitionId="+
         processDefinitionId
 
     }).then(function(response) {
